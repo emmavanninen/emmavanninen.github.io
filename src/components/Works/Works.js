3ca09830da9a243d320ded1fc659c21bd974c624
emmavanninen <emmavanninen@gmail.com>
2020-03-04 15:29:33 -0500

    card styling

--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -12,61 +12,104 @@ export default class Works extends Component {
               <div className='card'>
                 <img src='./chatapp.jpg' className='card-img' />
                 <div className='card-body'>
-                  <h5>Work X</h5>
+                  <h5>ChatAway</h5>
+                  <h6>(Fullstack App)</h6>
                   <div>
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                     Maiores optio nisi ex nihil, accusamus repellat sequi. Ab,
                     unde voluptates, ipsa id consectetur culpa, rerum ullam quia
                     tempore cumque ratione in.
                   </div>
-                  <a href='!#' target='_blank' className='work-link'>
-                    Check it out
-                  </a>
+                  <br />
+                  <div>
+                    Check it out:
+                    <br />
+                    <a href='https://chataway.surge.sh/' target='_blank'>
+                      <img src='./globe.png' alt='website icon' />
+                    </a>
+                    <a
+                      href='https://github.com/emmavanninen/chat-app-frontend'
+                      target='_blank'
+                    >
+                      <img src='./github.png' alt='website icon' />
+                    </a>
+                  </div>
                 </div>
               </div>
               <div className='card'>
                 <img src='./dogdaycare.jpg' className='card-img' />
                 <div className='card-body'>
-                  <h5>Work X</h5>
+                  <h5>Dog Daycare</h5>
+                  <h6>(Layout on React)</h6>
                   <div>
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                     Maiores optio nisi ex nihil, accusamus repellat sequi. Ab,
                     unde voluptates, ipsa id consectetur culpa, rerum ullam quia
                     tempore cumque ratione in.
                   </div>
-                  <a href='!#' target='_blank' className='work-link'>
-                    Check it out
-                  </a>
+                  <br />
+                  <div>
+                    Check it out:
+                    <br />
+                    <a href='http://kartanonkirsut.surge.sh/' target='_blank'>
+                      <img src='./globe.png' alt='website icon' />
+                    </a>
+                    <a
+                      href='https://github.com/emmavanninen/kartanonkirsut-frontend'
+                      target='_blank'
+                    >
+                      <img src='./github.png' alt='website icon' />
+                    </a>
+                  </div>
                 </div>
               </div>
               <div className='card'>
                 <img src='./venuebooking.jpg' className='card-img' />
                 <div className='card-body'>
-                  <h5>Work X</h5>
+                  <h5>Venue Booking Calendar</h5>
+                  <h6>(Fullstack practice project)</h6>
                   <div>
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                     Maiores optio nisi ex nihil, accusamus repellat sequi. Ab,
                     unde voluptates, ipsa id consectetur culpa, rerum ullam quia
                     tempore cumque ratione in.
                   </div>
-                  <a href='!#' target='_blank' className='work-link'>
-                    Check it out
-                  </a>
+                  <br />
+                  <div>
+                    Check it out:
+                    <br />
+                    <a
+                      href='https://venue-booking-calendar-project.surge.sh/'
+                      target='_blank'
+                    >
+                      <img src='./globe.png' alt='website icon' />
+                    </a>
+                    <a
+                      href='https://github.com/emmavanninen/calendar-reactjs'
+                      target='_blank'
+                    >
+                      <img src='./github.png' alt='website icon' />
+                    </a>
+                  </div>
                 </div>
               </div>
               <div className='card'>
                 <img src='./togowidget.jpg' className='card-img' />
                 <div className='card-body'>
-                  <h5>Work X</h5>
+                  <h5>Todo App</h5>
+                  <h6>(Vanilla Javascript practice)</h6>
+                  <div>Productivity Widget</div>
+                  <br />
                   <div>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Maiores optio nisi ex nihil, accusamus repellat sequi. Ab,
-                    unde voluptates, ipsa id consectetur culpa, rerum ullam quia
-                    tempore cumque ratione in.
+                    Check it out:
+                    <br />
+                    <a
+                      href='https://github.com/emmavanninen/todo-list'
+                      target='_blank'
+                    >
+                      <img src='./github.png' alt='website icon' />
+                    </a>
                   </div>
-                  <a href='!#' target='_blank' className='work-link'>
-                    Check it out
-                  </a>
                 </div>
               </div>
             </div>
