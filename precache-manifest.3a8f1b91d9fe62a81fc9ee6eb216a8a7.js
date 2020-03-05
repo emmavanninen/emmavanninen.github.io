287c4ed9272cabd2914947789656e59f7ff341db
emmavanninen <emmavanninen@gmail.com>
2020-03-05 15:42:12 -0500

    Updates

---
+++ precache-manifest.3a8f1b91d9fe62a81fc9ee6eb216a8a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0bb7c467a2fc3b349db59a525b9afaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f6dd35e8a56b20905b8",
+    "url": "/static/css/main.48a92e95.chunk.css"
+  },
+  {
+    "revision": "8988e72d36390bebbb83",
+    "url": "/static/js/2.00c0a123.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.00c0a123.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5f6dd35e8a56b20905b8",
+    "url": "/static/js/main.684f68a2.chunk.js"
+  },
+  {
+    "revision": "c0d7c58e510620f2f64c",
+    "url": "/static/js/runtime-main.7b61e083.js"
+  },
+  {
+    "revision": "f0748cfdb3a08336d4f13627a7e501af",
+    "url": "/static/media/cv2020.f0748cfd.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.46a567f6407b9954e68492394f027f14.js"
+  "/precache-manifest.3a8f1b91d9fe62a81fc9ee6eb216a8a7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
