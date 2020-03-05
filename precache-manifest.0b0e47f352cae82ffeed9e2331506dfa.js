8d527d30b11b57ca3922f6633e404efdada767f6
emmavanninen <emmavanninen@gmail.com>
2020-03-05 15:45:37 -0500

    Updates

---
+++ precache-manifest.0b0e47f352cae82ffeed9e2331506dfa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e178f317c8ca2c7621a70c94b79a3dc",
+    "url": "/emmavanninen/emmavanninen.github.io/index.html"
+  },
+  {
+    "revision": "5f6dd35e8a56b20905b8",
+    "url": "/emmavanninen/emmavanninen.github.io/static/css/main.48a92e95.chunk.css"
+  },
+  {
+    "revision": "8988e72d36390bebbb83",
+    "url": "/emmavanninen/emmavanninen.github.io/static/js/2.00c0a123.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/emmavanninen/emmavanninen.github.io/static/js/2.00c0a123.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5f6dd35e8a56b20905b8",
+    "url": "/emmavanninen/emmavanninen.github.io/static/js/main.684f68a2.chunk.js"
+  },
+  {
+    "revision": "b4912b28332b74d08bf2",
+    "url": "/emmavanninen/emmavanninen.github.io/static/js/runtime-main.ec2c622c.js"
+  },
+  {
+    "revision": "f0748cfdb3a08336d4f13627a7e501af",
+    "url": "/emmavanninen/emmavanninen.github.io/static/media/cv2020.f0748cfd.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3a8f1b91d9fe62a81fc9ee6eb216a8a7.js"
+  "/emmavanninen/emmavanninen.github.io/precache-manifest.0b0e47f352cae82ffeed9e2331506dfa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/emmavanninen/emmavanninen.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
