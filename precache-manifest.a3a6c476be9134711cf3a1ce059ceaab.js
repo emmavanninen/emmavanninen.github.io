5f0890ae5055be85f76320005dceb91c84d71e89
emmavanninen <emmavanninen@gmail.com>
2020-03-05 10:19:14 -0500

    Updates

---
+++ precache-manifest.a3a6c476be9134711cf3a1ce059ceaab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c5dc6f37a94162a4c9ec840beb895bd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2345f14de037b68fd5ca",
+    "url": "/portfolio/static/css/main.0ecc8a38.chunk.css"
+  },
+  {
+    "revision": "be105c8cbe7db162086d",
+    "url": "/portfolio/static/js/2.9a20e5ae.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/portfolio/static/js/2.9a20e5ae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2345f14de037b68fd5ca",
+    "url": "/portfolio/static/js/main.e6ccba94.chunk.js"
+  },
+  {
+    "revision": "6fef3673099b4dcdb973",
+    "url": "/portfolio/static/js/runtime-main.fed7eca6.js"
+  },
+  {
+    "revision": "dfbbdb6d39631f09ecdc442fbcecf362",
+    "url": "/portfolio/static/media/cv2020.dfbbdb6d.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e6d1373df850d95d0ca93d923129b04f.js"
+  "/portfolio/precache-manifest.a3a6c476be9134711cf3a1ce059ceaab.js"
 );
 
 self.addEventListener('message', (event) => {
